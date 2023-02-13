8ca9c3e1e1dae65ed6a165db2d7a7d28a7ea2e71
bro <percy.nguyen7@gmail.com>
2023-02-12 20:05:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -113,16 +113,16 @@ tailsBtn.addEventListener(`click`,()=>{
 
 coinWrapper.addEventListener(`click`,()=>{
     
-    if (playerBetInput.value === `` && !roulettePlaying){
-        alert(`Place your bet now!`);
-    }
-    else if (coinGuess === `none`){
-        console.log('test')
-        alert(`Select heads or tails!`);
-    }
+ 
     // if conditions met, you may spin
     if (coinGuess != `none` && playerBetInput.value != `` && !roulettePlaying){
-       
+        if (playerBetInput.value === `` && !roulettePlaying){
+            alert(`Place your bet now!`);
+        }
+        else if (coinGuess === `none`){
+            console.log('test')
+            alert(`Select heads or tails!`);
+        }
         if (playerBet > playerCash){
             alert(`You can't bet more than your current balance`)
         }
